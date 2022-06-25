49288a1dc6c0191eef4a887b77be36cd6978d126
asp <dr.profee@yandex.ru>
2022-06-25 13:39:21 +0300

    Dz11 state

---
+++ src/Dz10State/RunCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OtusDZ\Src\Dz10State;
+
+use OtusDZ\Src\Dz4MacroCommand\Commands\Command;
+
+class RunCommand implements Command
+{
+    public function execute()
+    {
+
+    }
+}
---
+++ tests/Dz10State/StateTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Dz10State;
+
+use OtusDZ\Src\Dz10State\CommandProcessor;
+use OtusDZ\Src\Dz10State\HardStopCommand;
+use OtusDZ\Src\Dz10State\MoveToCommand;
+use OtusDZ\Src\Dz10State\RunCommand;
+use OtusDZ\Src\Dz5IoC\IoC;
+use OtusDZ\Src\Dz5IoC\IoCRegister;
+use OtusDZ\Src\SomeNotes\Queue\src\QueueManager;
+use PHPUnit\Framework\TestCase;
+
+class StateTest extends TestCase
+{
+    public QueueManager $queueMock;
+
+    public function setUp(): void
+    {
+        $this->queueMock = $this->createMock(QueueManager::class);
+        IoC::resolve(IoC::IOC_REGISTER, IoC::IOC_REGISTER, function () {
+            return new IoCRegister();
+        })->execute();
+        IoC::resolve(IoC::IOC_REGISTER, QueueManager::class, function () {
+            return $this->queueMock;
+        })->execute();
+        parent::setUp();
+    }
+
+    public function testHardStop()
+    {
+        $this->queueMock->method('dequeue')->willReturn(new HardStopCommand());
+        $cp = new CommandProcessor();
+        $cp->run();
+        $this->assertEquals(CommandProcessor::HARD_STOP_STATE, $cp->sm->getCurrentState()->getName());
+    }
+
+    public function testRunCommand()
+    {
+        $this->queueMock->method('dequeue')->willReturn(new RunCommand());
+        $cp = new CommandProcessor();
+        $cp->run();
+        $this->assertEquals(CommandProcessor::USUAL_STATE, $cp->sm->getCurrentState()->getName());
+    }
+
+    public function testMoveTo()
+    {
+        $this->queueMock->method('dequeue')->willReturn(new MoveToCommand());
+        $cp = new CommandProcessor();
+        $cp->run();
+        $this->assertEquals(CommandProcessor::MOVE_TO_STATE, $cp->sm->getCurrentState()->getName());
+    }
+}
